bab2bb3b017eab0f9b7106196792c7f9802dd0a1
Roland Zerek <rolandz@poczta.fm>
2019-03-06 08:00:35 +0100

    Added test case when repo doesn't find entity of the given id

--- src/DDD/Domain/Repository.cs
+++ src/DDD/Domain/Repository.cs
@@ -5,47 +5,47 @@ using System.Reflection;
 
 namespace DDD.Domain
 {
-    public class Repository<TEntity, TId> where TEntity : AggregateRoot<TId>
-    {
-        private readonly Func<TId, IEnumerable<Event>> historyProvider;
-        private readonly Action<IEnumerable<Event>> persistEvents;
+	public class Repository<TEntity, TId> where TEntity : AggregateRoot<TId>
+	{
+		private readonly Func<TId, IEnumerable<Event>> historyProvider;
+		private readonly Action<IEnumerable<Event>> persistEvents;
 
-        public Repository(
-            Func<TId, IEnumerable<Event>> historyProvider,
-            Action<IEnumerable<Event>> persistEvents)
-        {
-            this.historyProvider = historyProvider ?? throw new ArgumentNullException(nameof(historyProvider));
-            this.persistEvents = persistEvents ?? throw new ArgumentNullException(nameof(persistEvents));
-        }
+		public Repository(
+				Func<TId, IEnumerable<Event>> historyProvider,
+				Action<IEnumerable<Event>> persistEvents)
+		{
+			this.historyProvider = historyProvider ?? throw new ArgumentNullException(nameof(historyProvider));
+			this.persistEvents = persistEvents ?? throw new ArgumentNullException(nameof(persistEvents));
+		}
 
-        public TEntity GetItemById(TId id)
-        {
-            var history = historyProvider.Invoke(id);
-            if (!history.Any())
-            {
-                throw new KeyNotFoundException($"Instance with id {id} was not found");
-            }
-            var ctor = GetConstructor();
-            var instance = (TEntity)ctor.Invoke(null);
-            foreach (var e in history)
-            {
-                instance.ApplyEvent(e);
-            }
-            return instance;
-        }
+		public TEntity GetItemById(TId id)
+		{
+			var history = historyProvider.Invoke(id);
+			if (!history.Any())
+			{
+				throw new KeyNotFoundException($"Instance with id {id} was not found");
+			}
+			var ctor = GetConstructor();
+			var instance = (TEntity)ctor.Invoke(null);
+			foreach (var e in history)
+			{
+				instance.ApplyEvent(e);
+			}
+			return instance;
+		}
 
-        private static ConstructorInfo GetConstructor()
-        {
-            return typeof(TEntity)
-                .GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance)
-                .Where(c => c.GetParameters().Length == 0)
-                .FirstOrDefault();
-        }
+		private static ConstructorInfo GetConstructor()
+		{
+			return typeof(TEntity)
+				.GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance)
+				.Where(c => c.GetParameters().Length == 0)
+				.FirstOrDefault();
+		}
 
-        public void Save(TEntity item)
-        {
-            persistEvents.Invoke(item.GetUncommittedChanges());
-            item.ClearUncommittedChanges();
-        }
-    }
+		public void Save(TEntity item)
+		{
+			persistEvents.Invoke(item.GetUncommittedChanges());
+			item.ClearUncommittedChanges();
+		}
+	}
 }
